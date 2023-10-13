dc7961cd174fcf3de105cc720a3482862cc7f9e3
Maxim Yakovlev <yakovlev@cae-fidesys.com>
2023-10-13 14:56:04 +0300

    fix CUDA for VS 20222

--- eff_props_2D.m
+++ eff_props_2D.m
@@ -6,12 +6,12 @@ colormap jet
 initLoadValue = -0.000015;
 addLoadValueStep = -0.000025;
 loadType = [4.0, -2.0, 0.0];
-nGrid = 64;
+nGrid = 32;
 nTimeSteps = 1;
 nTasks = 3;
 nIter = 500000;
 eIter = 1.0e-10;
-device = 1;
+device = 0;
 N = 3;
 needCPUcalculation = false;
 needCompareStatic = false;
@@ -30,8 +30,8 @@ Sxx = get_sigma_2D(addLoadValueStep, loadType, nGrid, nTimeSteps, nIter, eIter, 
 
 % GPU CALCULATION
 outname = ['a', int2str(device)];
-system(['nvcc -O 3 -o ', outname, ' -DDEVICE_IDX=', int2str(device), ' -DNL=', int2str(nTasks), ' -DNGRID=', int2str(nGrid), ' -DNITER=', int2str(nIter), ' -DEITER=', num2str(eIter), ' -DNPARS=', int2str(11), ' EffPlast2D.cu main.cu']);
-system(['.\', outname, '.exe ', num2str(initLoadValue), ' ', num2str(loadType(1)), ' ', num2str(loadType(2)), ' ', num2str(loadType(3)), ' ', num2str(nTimeSteps), ' ' num2str(addLoadValueStep)]);
+system(['nvcc -O 3 -allow-unsupported-compiler -o ', outname, ' -DDEVICE_IDX=', int2str(device), ' -DNL=', int2str(nTasks), ' -DNGRID=', int2str(nGrid), ' -DNITER=', int2str(nIter), ' -DEITER=', num2str(eIter), ' -DNPARS=', int2str(11), ' EffPlast2D.cu main.cu'])
+system(['.\', outname, '.exe ', num2str(initLoadValue), ' ', num2str(loadType(1)), ' ', num2str(loadType(2)), ' ', num2str(loadType(3)), ' ', num2str(nTimeSteps), ' ' num2str(addLoadValueStep)])
 
 cd data
 
