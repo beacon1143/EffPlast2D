c28abcc116ba876d80b5b0713d1c01b66ce426a4
beacon1143 <beacon1143@gmail.com>
2024-06-10 18:10:07 +0300

    check if file exists

--- read_data_2D.m
+++ read_data_2D.m
@@ -1,7 +1,13 @@
-function [field] = read_data_2D(file_name, size, nX, nY)
-  fil = fopen(strcat(file_name, '_', int2str(size), '_.dat'), 'rb');
-  field = fread(fil, 'double');
-  fclose(fil);
-  field = reshape(field, nX, nY);
-  field = transpose(field);
+function [field] = read_data_2D(var_name, size, nX, nY)
+  file_name = strcat(var_name, '_', int2str(size), '_.dat');
+  if isfile(file_name)
+    fil = fopen(file_name, 'rb');
+    field = fread(fil, 'double');
+    fclose(fil);
+    field = reshape(field, nX, nY);
+    field = transpose(field);
+  else
+    disp(strjoin({'Error! File', file_name, 'does not exist!'}));
+    field = zeros(nX, nY);
+  end
 end % function
